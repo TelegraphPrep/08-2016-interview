80d575f8ef5169d2e321aefec24611d367ee95d7
Kristina McElveen <kmcelveen.sp26@gmail.com>
2016-04-24 14:06:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Interviewing
+
+Whether you are interviewing for a software engineering position or interviewing to get into a bootcamp, both processes require technical ability. But technical ability is not the only skill that employers/bootcamps look for in a potential candidate. As individuals, we tend to focus on our hard skills at the expense of soft skills. While hard skills show whether you are qualified for a job/bootcamp, your soft skills show whether you are suited for a job, workplace or bootcamp. 
+
+  Important Soft Skills:
+
+  * Critical Thinking
+  * Communication
+  * Listening
+  * Adaptability
+  * Positive Attitude
+  * Great Work Ethic
+
+
+This sprint is focused on practicing some the important soft skills that are listed above which will put you in a better position to interview for Telegraph Academy. You, along with your pair, will take turns interviewing one another for 60 minutes total. You will take turns being the interviewer and the interviewee for 25 minutes each with 5 minutes of feedback time which brings it to 30 minutes for each individual. There are algorithms that are listed in prompts.js. Lets take a look at the roles:
+
+
+##Interviewer:
+  * Choose the algorithm
+  * Explain the algorithm to the interviewee
+  * Help the interviewee by being Stack-Overflow or Google should they have questions
+  * Guide the interviewee, DO NOT GIVE THE ANSWER!
+  * Question the interviewee's logic/approach (Well, why did you decide to approach the problem this way?)
+
+
+## Interviewee:
+  * Listen to the interviewer
+  * Understand the problem
+  * Ask questions!
+  * Communicate like an engineer
+  * Think through the problem logically
+  * Provide the most MVP (Minimum Viable Product) solution first.
+
+## The Rules:
+  * You cannot use the the internet. You must use your interviewer as if they are Google and Stack-Overflow
+  * You cannot use the browser console or any console.
+  * You cannot use syntax highlighting
+  * You can use [codeshare.io](https://codeshare.io/) or [kobra.io](https://kobra.io/#/)
+
---
+++ prompts.js
@@ -0,0 +1,136 @@
+// Interviewers, choose a problem from the options below:
+
+
+
+
+
+
+/*
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, 
+ignoring punctuation, case, and spacing.
+
+Return true if the given string is a palindrome. Otherwise, return false.
+
+palindrome("race CAR"); // true
+
+*Note*:
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
+
+*/
+
+
+var isPalindrome = function(str) {
+  // Your Code Here
+}
+
+
+
+/**********************************************************************************************************/
+
+
+
+/*
+findPrimes takes a collection of numbers and returns only the numbers that are Prime.
+
+var somePrimes = [7, 22, 9, 13, 71, 39];
+var onlyPrimes = findPrimes(somePrimes);
+console.log(onlyPrimes) --> [7, 13, 71];
+
+*/
+
+
+var findPrimes = function(collection){
+    // Your Code Here
+
+};
+
+
+
+
+/*********************************************************************************************************************/
+
+
+/*
+  
+  Create a Mad Libs word game function that will create a 
+  "Fill in the Blanks" style sentence.
+
+  Use the native Reduce method to solve this problem
+
+  wordBlanks("Kristina", "fancy", "laugh", "Deliberately"); // Kristina fancy laugh Deliberately
+  
+*/
+
+
+
+var wordBlanks = function(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your Code Here
+}
+
+
+
+
+
+/*************************************************************************************************************************/
+
+
+
+
+/*
+
+  Return a string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+  
+  titleCaseString("Shanna can do a handstand, Kristina can't"); // Shanna Can Do A Handstand, Kristina Can't
+
+  
+*/
+
+
+var titleCaseString = function (string) {
+  //Your Code Here
+}  
+  
+
+
+
+/****************************************************************************************************************************/
+
+
+
+/*
+
+collectKeys takes a collection and returns an array filled with all of they keys inside of the collection.
+
+var obj = {course: 'Telegraph Prep+', best: true};
+collectKeys(obj) --> ['course', 'best'];
+
+*/
+
+
+var collectKeys = function(collection){
+  // Your Code Here
+};
+
+/*******************************************************************************************/
+
+
+/*
+  
+  Truncate a string (str- first argument) if it is longer than the given maximum string length (num- second argument). Return the truncated string with a "..." ending.
+
+  Note that the three dots at the end add to the string length.
+
+  If the num is less than or equal to 3, then the length of the three dots is not added to the string length.
+
+  truncate("A-tisket a-tasket A green and yellow basket", 11); //"A-ticket..."
+
+
+*/
+
+
+var truncate = function(str, num) {
+  // Your Code Here
+  
+}
+
